4f1859a4eddb6363ea0df8e8e8ef25770cc68ccb
Mochi <mochi@ml1.net>
2019-10-07 23:34:49 +0100

    server cmd

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/logrusorgru/aurora"
+
+	"github.com/mochi-co/mqtt"
+	"github.com/mochi-co/mqtt/listeners"
+)
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		done <- true
+	}()
+
+	fmt.Println(aurora.Magenta("Mochi MQTT Broker initializing..."))
+
+	server := mqtt.New()
+	tcp := listeners.NewTCP("t1", ":1882")
+	log.Println(tcp)
+	err := server.AddListener(tcp, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start broker...
+	go server.Serve()
+	fmt.Println(aurora.BgMagenta("  Started!  "))
+
+	// Wait for signals...
+	<-done
+	fmt.Println(aurora.BgRed("  Caught Signal  "))
+
+	// End gracefully.
+	server.Close()
+	fmt.Println(aurora.BgGreen("  Finished  "))
+
+}
