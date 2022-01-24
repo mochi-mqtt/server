ae3aa0d3facfd35c46130ec264b0f9a87c9b2e06
mochi <mochimou@icloud.com>
2022-01-24 17:36:50 +0000

    Add OnConnect event hook

--- server/events/events.go
+++ server/events/events.go
@@ -7,6 +7,8 @@ import (
 
 type Events struct {
 	OnMessage // published message receieved.
+	OnMessage    // published message receieved.
+	OnConnect    // client connected.
 }
 
 type Packet packets.Packet
@@ -34,3 +36,6 @@ func FromClient(cl *clients.Client) Client {
 // This function will block message dispatching until it returns. To minimise this,
 // have the function open a new goroutine on the embedding side.
 type OnMessage func(Client, Packet) (Packet, error)
+
+// OnConnect is called when a client successfully connects to the broker.
+type OnConnect func(Client, Packet)
