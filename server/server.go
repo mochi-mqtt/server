5726880095a197a7cc70b5c7311f09f93f26f223
mochi <mochimou@icloud.com>
2022-03-31 17:36:03 +0100

    fix inflight key reference

--- server/server.go
+++ server/server.go
@@ -565,7 +565,7 @@ func (s *Server) publishToSubscribers(pk packets.Packet) {
 				if s.Store != nil {
 					s.onStorage(client, s.Store.WriteInflight(persistence.Message{
 						ID:          persistentID(client, out),
-						T:           persistence.KRetained,
+						T:           persistence.KInflight,
 						FixedHeader: persistence.FixedHeader(out.FixedHeader),
 						TopicName:   out.TopicName,
 						Payload:     out.Payload,
@@ -824,7 +824,7 @@ func (s *Server) ResendClientInflight(cl *clients.Client, force bool) error {
 		if s.Store != nil {
 			s.onStorage(cl, s.Store.WriteInflight(persistence.Message{
 				ID:          persistentID(cl, tk.Packet),
-				T:           persistence.KRetained,
+				T:           persistence.KInflight,
 				FixedHeader: persistence.FixedHeader(tk.Packet.FixedHeader),
 				TopicName:   tk.Packet.TopicName,
 				Payload:     tk.Packet.Payload,
