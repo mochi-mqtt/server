aac6d699daf7d58555805a9e706e700256570cfe
thedevop <60499013+thedevop@users.noreply.github.com>
2023-04-21 13:20:46 -0700

    Ensure to close client WriteLoop (#193)
    * Ensure client WriteLoop is closed
    
    * Ensure to close client WriteLoop

--- server.go
+++ server.go
@@ -374,7 +374,6 @@ func (s *Server) attachClient(cl *Client, listener string) error {
 	s.Log.Debug().Str("client", cl.ID).Err(err).Str("remote", cl.Net.Remote).Str("listener", listener).Msg("client disconnected")
 	expire := (cl.Properties.ProtocolVersion == 5 && cl.Properties.Props.SessionExpiryIntervalFlag && cl.Properties.Props.SessionExpiryInterval == 0) || (cl.Properties.ProtocolVersion < 5 && cl.Properties.Clean)
 	s.hooks.OnDisconnect(cl, err, expire)
-	close(cl.State.outbound)
 
 	if expire && atomic.LoadUint32(&cl.State.isTakenOver) == 0 {
 		cl.ClearInflights(math.MaxInt64, 0)
