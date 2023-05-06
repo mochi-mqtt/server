797d75cb343a40f35a82b66891b5aabb079829d0
mochi-co <moumochi@icloud.com>
2023-05-06 14:32:42 +0100

    update server version

--- server.go
+++ server.go
@@ -26,7 +26,7 @@ import (
 )
 
 const (
-	Version                       = "2.2.8" // the current server version.
+	Version                       = "2.2.9" // the current server version.
 	defaultSysTopicInterval int64 = 1       // the interval between $SYS topic publishes
 )
 
