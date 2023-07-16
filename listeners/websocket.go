aec29e350eaebfe447560222392eab048d9d55a2
Ian Rose <ianrose14@gmail.com>
2023-07-16 15:36:15 -0400

    Fix websocket reads for packets > 1 buffer size (#260)

--- listeners/websocket.go
+++ listeners/websocket.go
@@ -99,7 +99,7 @@ func (l *Websocket) handler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer c.Close()
 
-	err = l.establish(l.id, &wsConn{c.UnderlyingConn(), c})
+	err = l.establish(l.id, &wsConn{Conn: c.UnderlyingConn(), c: c})
 	if err != nil {
 		l.log.Warn().Err(err).Send()
 	}
@@ -135,25 +135,41 @@ func (l *Websocket) Close(closeClients CloseFn) {
 type wsConn struct {
 	net.Conn
 	c *websocket.Conn
+
+	// reader for the current message (may be nil)
+	r io.Reader
 }
 
 // Read reads the next span of bytes from the websocket connection and returns the number of bytes read.
 func (ws *wsConn) Read(p []byte) (int, error) {
-	op, r, err := ws.c.NextReader()
-	if err != nil {
-		return 0, err
+	if ws.r == nil {
+		op, r, err := ws.c.NextReader()
+		if err != nil {
+			return 0, err
+		}
+
+		if op != websocket.BinaryMessage {
+			err = ErrInvalidMessage
+			return 0, err
+		}
+
+		ws.r = r
 	}
 
-	if op != websocket.BinaryMessage {
-		err = ErrInvalidMessage
-		return 0, err
-	}
-
-	var n, br int
+	var n int
 	for {
-		br, err = r.Read(p[n:])
+		// buffer is full, return what we've read so far
+		if n == len(p) {
+			return n, nil
+		}
+
+		br, err := ws.r.Read(p[n:])
 		n += br
 		if err != nil {
+			// when ANY error occurs, we consider this the end of the current message (either because it really is, via
+			// io.EOF, or because something bad happened, in which case we want to drop the remainder)
+			ws.r = nil
+
 			if errors.Is(err, io.EOF) {
 				err = nil
 			}
