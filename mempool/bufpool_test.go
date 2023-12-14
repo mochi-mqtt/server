dabeecdbdb2581c97353ada5a53aa76a479fecd1
thedevop <60499013+thedevop@users.noreply.github.com>
2023-12-14 11:48:56 -0800

    Add benchmark for bufpool

--- mempool/bufpool_test.go
+++ mempool/bufpool_test.go
@@ -1,6 +1,7 @@
 package mempool
 
 import (
+	"bytes"
 	"reflect"
 	"runtime/debug"
 	"testing"
@@ -50,4 +51,46 @@ func TestBufferWithCap(t *testing.T) {
 	buf = bp.Get()
 	require.Equal(t, 0, buf.Len())
 	require.Equal(t, 0, buf.Cap())
+}
+
+func BenchmarkBufferPool(b *testing.B) {
+	bp := NewBuffer(0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := bp.Get()
+		b.WriteString("this is a test")
+		bp.Put(b)
+	}
+}
+
+func BenchmarkBufferPoolWithCapLarger(b *testing.B) {
+	bp := NewBuffer(64 * 1024)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := bp.Get()
+		b.WriteString("this is a test")
+		bp.Put(b)
+	}
+}
+
+func BenchmarkBufferPoolWithCapLesser(b *testing.B) {
+	bp := NewBuffer(10)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := bp.Get()
+		b.WriteString("this is a test")
+		bp.Put(b)
+	}
+}
+
+func BenchmarkBufferWithoutPool(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := new(bytes.Buffer)
+		b.WriteString("this is a test")
+		_ = b
+	}
 }
